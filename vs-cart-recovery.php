a630f280ac0be9bd7b012d3dec90dba5abef31fc
jbxxnn <jbxxnn6@gmail.com>
2023-06-19 12:48:39 +0100

    update

---
+++ vs-cart-recovery.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Vs Abandoned Cart Recovery
+Plugin URI: https://www.jbxxn.com/vs-abandoned-cart-recovery
+Description: This plugin helps store owners to recover abandoned carts by sending follow up emails.
+Version: 1.0
+Author: jibrin faruna
+Author URI: https://www.jbxxn.com/
+Text Domain: vs-abandoned-cart-recovery
+Domain Path: /languages/
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
